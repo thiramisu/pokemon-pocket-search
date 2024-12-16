511b4ea44ac734c1d4522946cd8361c2d6ddcb1e
tiramisu_oTATo <xxx.partyo@gmail.com>
2024-12-17 05:57:05 +0900

    PWA化
    vite-plugin-pwa を導入
    manifest.json 相当の内容を vite.config.ts へ移動

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import chokidar from "chokidar";
+import { VitePWA } from "vite-plugin-pwa";
 import { processJSON } from "./src/services/dataProcessing";
 
 // https://vite.dev/config/
@@ -18,6 +19,43 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "apple-touch-icon.png",
+        "dark_mode.svg",
+        "favicon-96x96.png",
+        "favicon.ico",
+        "favicon.svg",
+        "fullscreen_portrait.svg",
+        "light_mode.svg",
+        "tune.svg",
+      ],
+      injectRegister: "auto",
+      manifest: {
+        name: "ポケポケ カード検索",
+        short_name: "PP Search",
+        description: "ポケポケのカードを検索できます。",
+        start_url: "/",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#81e9b0",
+        background_color: "#ff8585",
+        display: "standalone",
+      },
+    }),
     {
       name: "auto-process-json",
       buildStart() {

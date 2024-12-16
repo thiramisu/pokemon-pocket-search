fc4af327a23ffef98a2bf3ac0ad7bf600ded9820
thiramisu <xxx.partyo@gmail.com>
2024-12-16 19:23:09 +0000

    Deploying to gh-pages from @ thiramisu/pokemon-pocket-search@ae155a2f6a4fe6e9431a516a91ee9323187d93ec 🚀

--- src/const.ts
+++
@@ -1,6 +0,0 @@
-export const COLOR_LESS_JP = "無色";
-export const POKEMON_NO_WEAK = "なし";
-export const POKEMON_NAME_DATALIST_ID = "pokemon-name-data";
-export const PACK_NO_FILTERED = "すべて";
-export const PACK_SHARED = "（共通のみ）";
-export const SHARED_PACK = "（共通）";
